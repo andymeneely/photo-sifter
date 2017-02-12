f48909af5454c6f5650ac52ee02834793f5ad103
Andy Meneely <andy@se.rit.edu>
2017-02-12 00:21:53 -0500

    I think this is done.

--- sift.rb
+++ sift.rb
@@ -1,8 +1,9 @@
 require 'exif'
 require 'slop'
+require 'fileutils'
+require 'digest/sha2'
 
 opts = Slop.parse do |cli|
-  # cli.banner 'ruby sift.rb --input=my-messy-photos-dir --output=clean-dir'
   cli.on '--help' do
     puts o
     exit
@@ -12,15 +13,38 @@ opts = Slop.parse do |cli|
   cli.string 'output','-o', '--output', 'Output directory'
 end
 
+unless (Dir.exist?(opts['input'].to_s) && Dir.exist?(opts['output'].to_s))
+  puts 'Please specify the input and output directories with --input=dir and --output=dir'
+  exit
+end
+
 in_dir = File.expand_path(opts['input'])
-out_dir = File.epand_paht(opts['output'])
+out_dir = File.expand_path(opts['output'])
+puts "Indexing the output directory..."
+image_hashes = Dir["#{out_dir}/**/*.{jpg,JPG}"].inject({}) do |memo, f|
+  memo[Digest::SHA256.base64digest(File.read(f))] = true; memo
+end
 
-
-require 'byebug'; byebug
-
-Dir["#{in_dir)}/**/*.{jpg,JPG}"].each do |f|
+puts "Copying files..."
+Dir["#{in_dir}/**/*.{jpg,JPG}"].each do |f|
   puts "Processing: #{f}"
-  
+  exif = Exif::Data.new f
+  hash = Digest::SHA256.base64digest(File.read(f))
+  # Make the directory
+  dir = "#{out_dir}/#{exif.date_time_original.strftime('%Y-%m%b')}"
+  FileUtils.mkdir_p dir
+  if image_hashes[hash]
+    puts "  File already exists, ignoring...."
+    next
+  end
+  image_hashes[hash] = true
+  # Check for same filename (but different hash??)
+  out_f = "#{dir}/#{File.basename(f)}"
+  if File.exists? out_f
+    puts "  WARNING: Filename already exists, but different image. Adding hash to the filename..."
+    out_f << ".#{hash}.jpg"
+  end
+  FileUtils.cp f, out_f
 end
 
 puts "Done at #{Time.now}"
